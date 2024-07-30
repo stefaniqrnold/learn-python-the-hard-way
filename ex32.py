1436a03629a9db3b232f96ac5deeb686c6dffa2e
stefaniqrnold <stefaniqrnold@gmail.com>
2024-07-30 17:43:17 -0300

    batata

---
+++ ex32.py
@@ -0,0 +1,10 @@
+theCount = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in theCount:
+    print(f"This is count {number}")
+
+# same as above
+for fruit in fruits:
