4ed97b5e970c4e084b7bab6a13aab1a27b34ca53
stefaniqrnold <stefaniqrnold@gmail.com>
2024-08-02 16:08:27 -0300

    onibus

--- ex33.py
+++ ex33.py
@@ -1,14 +1,15 @@
-def while_loop_function(limit):
-    i = 0
+def for_loop_function(limit, increment):
     numbers = []
 
-    while i < limit:
+    for i in range(0, limit, increment):
         print(f"At the top i is {i}")
         numbers.append(i)
-        i += 1
         print(f"Numbers now: {numbers}")
         print(f"At the bottom i is {i}")
 
     print("The numbers: ")
     for num in numbers:
         print(num)
+
+for_loop_function(3, 1)
+for_loop_function(5, 2)
