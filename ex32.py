6a764ce006cd67f2eb7769822830b061486a6f09
stefaniqrnold <stefaniqrnold@gmail.com>
2024-08-02 15:13:39 -0300

    onibus

--- ex32.py
+++ ex32.py
@@ -8,3 +8,21 @@ for number in theCount:
 
 # same as above
 for fruit in fruits:
+    print(f"A fruit of type: {fruit}")
+
+# also we can go through mixed lists too
+for i in change:
+    print(f"I got {i}")
+
+# we can also build lists, first start with an empty one
+elements = []
+
+# then use the range function to do 0 to 5 counts
+for i in range(0, 6):
+    print(f"Adding {i} to the list.")
+    #append is a function that lists understand
+    elements.append(i)
+
+# now we can print them out too
+for i in elements:
+    print(f"Element was: {i}")
