9dace5eab176836826a1a9f9f42df2ccedf29af4
stefaniqrnold <stefaniqrnold@gmail.com>
2024-08-02 15:25:50 -0300

    onibus

--- ex32.py
+++ ex32.py
@@ -18,9 +18,12 @@ for i in change:
 elements = []
 
 # then use the range function to do 0 to 5 counts
+elements.extend(range(0, 6))
+print(f"Adding {elements} to the list.")
+
 for i in range(0, 6):
     print(f"Adding {i} to the list.")
-    #append is a function that lists understand
+    # append is a function that lists understand
     elements.append(i)
 
 # now we can print them out too
